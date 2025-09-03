bbf0363a73b6c873e64dd84827285d6fea74238a
Kirentheren Sanmugam <kirentheren@@gmail.com>
2025-09-03 17:27:01 +1000

    remove challenge comment

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -4,15 +4,6 @@ using System.Linq;
 
 namespace PointsBet_Backend_Online_Code_Test
 {
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
     public class StringFormatter
     {
         /// <summary>
