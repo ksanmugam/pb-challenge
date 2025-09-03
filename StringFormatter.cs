01c91428e4200ec01e04f5272ff9d35bc35c7faa
Kirentheren Sanmugam <kirentheren@@gmail.com>
2025-09-03 17:17:44 +1000

    initial commit

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -1,8 +1,9 @@
 ﻿using System.Text;
+using System;
+using System.Linq;
 
 namespace PointsBet_Backend_Online_Code_Test
 {
-
     /*
     Improve a block of code as you see fit in C#.
     You may make any improvements you see fit, for example:
@@ -14,21 +15,27 @@ namespace PointsBet_Backend_Online_Code_Test
     */
     public class StringFormatter
     {
-
-        //Code to improve
-        public static string ToCommaSepatatedList(string[] items, string quote)
+        /// <summary>
+        /// Converts an array of strings to a comma-separated list with each item wrapped in quotes.
+        /// </summary>
+        /// <param name="items">Array of strings to format</param>
+        /// <param name="quote">Quote character to wrap around each item</param>
+        /// <returns>Comma-separated string with quoted items</returns>
+        /// <exception cref="ArgumentNullException">Thrown when items array is null</exception>
+        /// <exception cref="ArgumentException">Thrown when items array is empty</exception>
+        public static string ToCommaSeparatedList(string[] items, string quote = "\"")
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+            // null check for the items array
+            if (items == null)
+                throw new ArgumentNullException(nameof(items), "Items array cannot be null");
 
-            if (items.Length > 1)
-            {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
-            }
+            // empty array check to prevent runtime errors
+            if (items.Length == 0)
+                throw new ArgumentException("Items array cannot be empty.", nameof(items));
 
-            return qry.ToString();
+            // handle null item by converting to empty strings
+            var quotedItems = items.Select(item => $"{quote}{item ?? string.Empty}{quote}");
+            return string.Join(", ", quotedItems);
         }
     }
 }
